705570c15cb6b1e2a3745018d0527091a05f46d4
Paul Roujansky <paul@roujansky.eu>
2019-06-28 13:47:05 +0200

    updated setup and pkg __init__

--- pydeco/__init__.py
+++ pydeco/__init__.py
@@ -1,2 +1,13 @@
-"""Pydeco pkg content."""
+# PyDeco.
+#
+# Author: Paul Roujansky <paul@roujansky.eu>
+# Last Change: June 28, 2019
+# URL: https://github.com/paulroujansky/pydeco
+
+"""The main module of the `pydeco` package."""
+
+# Library modules
 from .decorator import DecorateMethods
+
+# Semi-standard module versioning.
+__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
-#
+
+# Standard library modules.
 from setuptools import setup, find_packages
+import codecs
+import os
+import re
+import sys
 
 # platform specific dependency
 exclude = ['data', 'contrib', 'doc', 'tests']
@@ -8,15 +13,40 @@ exclude = ['data', 'contrib', 'doc', 'tests']
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+
+def get_absolute_path(*args):
+    """Transform relative pathnames into absolute pathnames."""
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
+
+
+def get_contents(*args):
+    """Get the contents of a file relative to the source distribution dir."""
+    with codecs.open(get_absolute_path(*args), 'r', 'UTF-8') as handle:
+        return handle.read()
+
+
+def get_version(*args):
+    """Extract the version number from a Python module."""
+    contents = get_contents(*args)
+    metadata = dict(re.findall('__([a-z]+)__ = [\'"]([^\'"]+)', contents))
+    return metadata['version']
+
+
 setup(
     name='pydeco',
-    description='Python Class Decorator',
+    version=get_version('pydeco', '__init__.py'),
+    description='Python Class Methods Decorator',
     url='https://github.com/paulroujansky/pydeco',
-    author='P Roujansky',
+    author='Paul Roujansky',
+    long_description=get_contents('README.rst'),
     author_email='paul@roujansky.eu',
-    license='UNLICENSED',
-    version='0.1',
-    include_package_data=True,
+    license='MIT',
+    test_suite='pydeco.tests',
     install_requires=requirements,
     packages=find_packages(exclude=exclude),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     zip_safe=False)
