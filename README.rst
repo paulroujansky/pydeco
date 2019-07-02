5e16acaa973236bfcfac67bd04b57e6490f5bd84
Paul Roujansky <paul@roujansky.eu>
2019-07-02 10:53:26 +0200

    updated doc and README

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 .. -*- mode: rst -*-
 
 
-|Travis|_ |Circle|_ |Codecov|_
+|Travis|_ |Circle|_ |Codecov|_ |PyPi|_ |License MIT|_
 
 .. |Travis| image:: https://api.travis-ci.org/paulroujansky/pydeco.png?branch=master
 .. _Travis: https://travis-ci.org/paulroujansky/pydeco/branches
@@ -11,6 +11,15 @@
 
 .. |Codecov| image:: https://codecov.io/gh/paulroujansky/pydeco/branch/master/graph/badge.svg
 .. _Codecov: https://codecov.io/gh/paulroujansky/pydeco
+
+.. |Python35| image:: https://img.shields.io/badge/python-3.5-blue.svg
+.. _Python35: https://badge.fury.io/py/scikit-learn
+
+.. |PyPi| image:: https://badge.fury.io/py/pydeco.svg
+.. _PyPi: https://badge.fury.io/py/pydeco
+
+.. |License MIT| image:: https://img.shields.io/badge/License-MIT-yellow.svg
+.. _License MIT: https://opensource.org/licenses/MIT
 
 PyDeco
 ======
@@ -31,6 +40,18 @@ To install the latest stable version of PyDeco, you can use pip in a terminal:
 
     pip install -U pydeco
 
+Source code
+^^^^^^^^^^^
+
+You can check the latest sources with the command:
+
+git clone https://github.com/paulroujansky/pydeco.git
+
+Contributing
+^^^^^^^^^^^
+
+To learn more about making a contribution to PyDeco, please see the `Contributing guide`_.
+
 License
 ^^^^^^^
 
@@ -42,3 +63,4 @@ This software is licensed under the `MIT license`_.
 .. _examples: https://github.com/paulroujansky/pydeco/tree/master/examples
 .. _PyDeco documentation: https://pydeco.readthedocs.io/en/latest/
 .. _MIT license: http://en.wikipedia.org/wiki/MIT_License
+.. _Contributing guide: https://pydeco.readthedocs.io/en/latest/contributing.html
--- pydeco/decorator.py
+++ pydeco/decorator.py
@@ -1,6 +1,9 @@
 """Decorator for class methods.
 
-...                                                                        TODO
+The herebelow class :class:`MethodsDecorator` enables to decorate multiple
+methods of a given class with custom decorators by simply decorating said class
+with it. One then needs to specify, as a dictionary, which decorator is used to
+decorate which methods.
 
 """
 
@@ -11,8 +14,34 @@ class MethodsDecorator(object):
     Parameters
     ----------
     mapping : dict
-        Mapping containing decorators as key and methods (as list of str) as
-        values (ex: {Timer(): ['fit', 'predict']})
+        Mapping containing decorators as key and methods (as str or a list of
+        str) as values (ex: ``{Timer(): ['fit', 'predict']}``)
+
+    Examples
+    --------
+    We assume here that a decorator named :class:`Timer` (used to compute
+    running time of called functions) is already implemented. The below code
+    applies such a decorator to the method :meth:`method_1` of class
+    :class:`MyClass` so as to compute running time every time :meth:`method_1`
+    is called.
+
+    >>> @MethodsDecorator(mapping={Timer(): 'method_1'})
+    >>> class MyClass():
+    >>>
+    >>>    def method_1(self, *args, **kwargs):
+    >>>       return
+
+    The "@" syntax is a proxy for the below implementation:
+
+    >>> class MyClass():
+    >>>
+    >>>    def method_1(self, *args, **kwargs):
+    >>>       return
+    >>>
+    >>> MyClass = MethodsDecorator(mapping={Timer(): 'method_1'})(MyClass)
+
+    See the examples section for more insights on how to use
+    :class:`MethodsDecorator`.
 
     """
 
@@ -26,11 +55,24 @@ class MethodsDecorator(object):
             self.mapping[decorator] = methods
 
     def __call__(self, cls):
-        """Call."""
+        """Return wrapped input class with decorated methods."""
         class MC(type):
-            """Decorating methods for the input class with given decorator."""
+            """Decorating methods for the input class with given decorator.
+
+            Attributes
+            ----------
+            decorated : bool
+                Indicates that current class is decorated.
+            class_decorator : type
+                Type of the class decorator used to decorate the current class.
+
+            """
 
             def __init__(cls, name, bases, dict):
+
+                cls.decorated = True  # indicates that the class is decorated
+                cls.class_decorator = self.__class__  # type of class decorator
+
                 for decorator, methods in self.mapping.items():
                     for method in methods:
                         if not hasattr(cls, method):
